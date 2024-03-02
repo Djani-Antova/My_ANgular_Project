46fb2130d6ec3a60c3257630996dffa669e62bf3
djaniAntova <djaniantova@gmail.com>
2024-03-03 01:24:15 +0200

    Update theme.ts

--- interior-forum/src/app/types/theme.ts
+++ interior-forum/src/app/types/theme.ts
@@ -6,6 +6,7 @@ export interface Theme {
     posts: any | string[] | Post[],
     _id: string,
     themeName: string,
+    // imageUrl: string - TODO check if I want it
     userId: UserId,
     created_at: string,
     updatedAt: string,
