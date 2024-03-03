9f5ba6bc5d3f86bb3a4ec387622bd8096411ca6c
djaniAntova <djaniantova@gmail.com>
2024-03-03 11:04:57 +0200

    Update

--- interior-forum/src/app/api.service.ts
+++ interior-forum/src/app/api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { Theme } from './types/theme';
+import { Post } from './types/post';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,23 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getThemes() {
-    const { apiUrl } = environment
-    
+    const { apiUrl } = environment    
     return this.http.get<Theme[]>(`${apiUrl}/themes`);
+  }
 
+  getLastThemes(limit?: number) {
+    const { apiUrl } = environment;
+    const limitCount = limit ? `?limit=${limit}` : '';
+    return this.http.get<Theme[]>(`${apiUrl}/themes${limitCount}`);
+  }
+
+  postComment(postText: string, themeId: string) {
+    return this.http.post<Post>(`/api/themes/${themeId}`, { postText })
   }
 
   getPosts() {
-    // const { apiUrl } = environment;
-    // return this.http.get<Post[]>(`${apiUrl}/posts`);
+    const { apiUrl } = environment;
+    return this.http.get<Post[]>(`${apiUrl}/posts`);
   }
 
 }
--- interior-forum/src/app/features/themes/themes-all/themes-all.component.ts
+++ interior-forum/src/app/features/themes/themes-all/themes-all.component.ts
@@ -8,13 +8,18 @@ import { ApiService } from 'src/app/api.service';
 })
 export class ThemesAllComponent implements OnInit {
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
     this.api.getThemes().subscribe((themes) => { //getThemes() is a method that returns observable => so we subscribe
       console.log(themes);
-      
-    })          
+
+      this.api.getPosts().subscribe((posts) => {  //TODO delete this line and specify for each theme
+        console.log(posts);
+
+      })
+
+    })
   }
 
 }
