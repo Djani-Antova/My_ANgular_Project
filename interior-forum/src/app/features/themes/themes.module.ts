f5bfbebcaca0cbb84f34595988118e7747574b32
djaniAntova <djaniantova@gmail.com>
2024-03-03 22:03:23 +0200

    Update themes.module.ts

--- interior-forum/src/app/features/themes/themes.module.ts
+++ interior-forum/src/app/features/themes/themes.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemesAllComponent } from './themes-all/themes-all.component';
+import { ThemeDetailsComponent } from './theme-details/theme-details.component';
 
 
 
 @NgModule({
   declarations: [
-    ThemesAllComponent
+    ThemesAllComponent,
+    ThemeDetailsComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ThemesAllComponent
+    ThemesAllComponent,
+    ThemeDetailsComponent
   ]
 })
 export class ThemesModule { }
