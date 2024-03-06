0eac55d446340b7276a50e9495edb8ede7208330
djaniAntova <djaniantova@gmail.com>
2024-03-06 22:36:14 +0200

    Add AppRoutingModule

--- interior-forum/src/app/app-routing.module.ts
+++ interior-forum/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './features/not-found/not-found.component';
+import { HomeComponent } from './features/home/home.component';
 
-const routes: Routes = [];
+// import { ErrorComponent } - TODO make it
+
+
+  const routes: Routes = [
+    {
+      path: '',
+      pathMatch: 'full',
+      redirectTo: '/home'
+    },
+    {
+      path: 'home', component: HomeComponent
+    },
+    {
+      path: 'error', component: NotFoundComponent
+    },
+    {
+      path: '**', component: NotFoundComponent
+    }
+  ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
