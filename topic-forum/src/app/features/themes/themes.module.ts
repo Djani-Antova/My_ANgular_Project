a497e68d0ea9fe6b210518889790e95be969978f
djaniAntova <djaniantova@gmail.com>
2024-03-10 17:47:30 +0200

    Edit error-page

--- topic-forum/src/app/features/themes/themes.module.ts
+++ topic-forum/src/app/features/themes/themes.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ThemesAllComponent } from './themes-all/themes-all.component';
+
 import { ThemeDetailsComponent } from './theme-details/theme-details.component';
-import { ThemesLimitComponent } from './themes-limit/themes-limit.component';
-import { ThemeRoutingModule } from './theme-routing.module';
 import { ThemeAddComponent } from './theme-add/theme-add.component';
+import { ThemesAllComponent } from './themes-all/themes-all.component';
+import { ThemeRoutingModule } from './theme-routing.module';
+import { ThemesLimitComponent } from './themes-limit/themes-limit.component';
 
 
 
 @NgModule({
   declarations: [
-    ThemesAllComponent,
     ThemeDetailsComponent,
+    ThemeAddComponent,
+    ThemesAllComponent,
     ThemesLimitComponent,
-    ThemeAddComponent
   ],
   imports: [
     CommonModule, ThemeRoutingModule
