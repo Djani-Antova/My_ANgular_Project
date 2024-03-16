df62489060592401c8655524c8d0ebf4b0825508
djaniAntova <djaniantova@gmail.com>
2024-03-16 23:04:01 +0200

    Create reactive driven login component

--- topic-forum/src/app/features/user/login/login.component.ts
+++ topic-forum/src/app/features/user/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { FormBuilder, NgForm, Validators } from '@angular/forms';
+import { emailValidator } from '../validators/email.validator';
+import { DOMAINS_FOR_EMAIL } from '../constants';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,5 +14,23 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  form = this.fb.group({
+    email: ['', [Validators.required, emailValidator(DOMAINS_FOR_EMAIL)],],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+  })
 
+  constructor(private userService: UserService, private router: Router, private fb: FormBuilder) {}
+
+  login():void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(this.form.value);
+    
+    this.userService.login();
+    this.router.navigate(['/']);
+  }
+
+  
 }
