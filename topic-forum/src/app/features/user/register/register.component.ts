08ea8a2c42c5f799d5387b701911a0b892190294
djaniAntova <djaniantova@gmail.com>
2024-03-16 17:02:25 +0200

    Create react driven form for register

--- topic-forum/src/app/features/user/register/register.component.ts
+++ topic-forum/src/app/features/user/register/register.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DOMAINS_FOR_EMAIL } from '../constants';
+import { emailValidator } from '../validators/email.validator';
+import { matchPasswordsValidator } from '../validators/match-passwords.validator';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],                                   
 })
 export class RegisterComponent {
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, emailValidator(DOMAINS_FOR_EMAIL)],],
+    passGroup: this.fb.group({
+      password: ['', [Validators.required]],
+      rePassword: ['', [Validators.required]],
+    }, {
+      validators: [matchPasswordsValidator('password', 'rePassword')]
+    }),
+  });
 
+  get passGroup() {
+    return this.form.get('passGroup');
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  register(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(this.form.value);
+  }
 }
