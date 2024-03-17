ed501d893eb63046392ff109c133e141027af138
djaniAntova <djaniantova@gmail.com>
2024-03-17 16:06:01 +0200

    Create theme-add form

--- topic-forum/src/app/features/themes/theme-add/theme-add.component.ts
+++ topic-forum/src/app/features/themes/theme-add/theme-add.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-theme-add',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./theme-add.component.css']
 })
 export class ThemeAddComponent {
+
+  constructor(private apiService: ApiService) {}
+
+  addNewTheme(form: NgForm): void {
+
+    if (form.invalid) {
+      return;
+    }
+    console.log(form.value);
+    
+    // const { themeName, postText } = form.value;
+
+    // this.subscription = this.apiService.addNewTheme(themeName, postText).subscribe({
+    //   next: () => {
+    //     this.router.navigate(["/themes"]);
+    //   },
+    //   error: (err) => this.errMessage = err.error.message
+    // })
+  }
 
 }
