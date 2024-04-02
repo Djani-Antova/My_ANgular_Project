ab63e1aad09099b7f075916382d145cd132bb013
djaniAntova <djaniantova@gmail.com>
2024-04-02 20:14:51 +0300

    Add error handling for theme-add component

--- topic-forum/src/app/features/themes/theme-add/theme-add.component.ts
+++ topic-forum/src/app/features/themes/theme-add/theme-add.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { ApiService } from 'src/app/api.service';
+import { Theme } from 'src/app/types/theme';
 
 @Component({
   selector: 'app-theme-add',
   templateUrl: './theme-add.component.html',
   styleUrls: ['./theme-add.component.css']
 })
-export class ThemeAddComponent {
+export class ThemeAddComponent implements OnDestroy {
+
+  theme: Theme | undefined;
+  subscription!: Subscription;
+  errMessage!: string;
 
   constructor(private apiService: ApiService, private router: Router) {}
+
 
   addNewTheme(form: NgForm): void {
 
@@ -19,17 +26,19 @@ export class ThemeAddComponent {
     }
        
     const { themeName, postText } = form.value;
-    this.apiService.addNewTheme(themeName, postText).subscribe(() => {
-      this.router.navigate(['/themes'])
+  
+    this.subscription = this.apiService.addNewTheme(themeName, postText).subscribe({
+      next: () => {
+        this.router.navigate(["/themes"]);
+      },
+      error: (err) => this.errMessage = err.error.message
     })
-    
+  }
 
-    // this.subscription = this.apiService.addNewTheme(themeName, postText).subscribe({
-    //   next: () => {
-    //     this.router.navigate(["/themes"]);
-    //   },
-    //   error: (err) => this.errMessage = err.error.message
-    // })
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 }
